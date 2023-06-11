d540cadba6e31ed054eab1673468f8688f43b60b
Cristian Paez <paezmartinez866@gmail.com>
2023-06-11 15:51:22 -0500

    Se deja codigo actualizado

--- Concesionario/src/main/java/Logica/Concesionario.java
+++ Concesionario/src/main/java/Logica/Concesionario.java
@@ -5,7 +5,8 @@
 package Logica;
 
 import Formularios.Frm_AgregarVehiculo;
-
+import Formularios.Frm_Venta;
+import java.util.Scanner;
 /**
  *
  * @author rzl10
@@ -13,8 +14,21 @@ import Formularios.Frm_AgregarVehiculo;
 public class Concesionario {
 
     public static void main(String[] args) {
-        Frm_AgregarVehiculo AV = new Frm_AgregarVehiculo();
-        AV.setVisible(true);
+        Scanner entrada = new Scanner(System.in);
+        int opc;
+        
+        System.out.println("Digite por favor si desea   Registrar vehiculo o Registrar venta");
+        opc = entrada.nextInt();
+        
+        if (opc = 1) {
+            Frm_AgregarVehiculo AV = new Frm_AgregarVehiculo();
+            AV.setVisible(true);
+        }
+        else {
+            Frm_Venta AgV = new Frm_Venta();
+            AgV.setVisible(true);
+        }
+        
         
     }
 }
