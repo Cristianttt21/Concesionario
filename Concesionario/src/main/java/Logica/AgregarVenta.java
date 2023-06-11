ac8e339de909e25da541c5027abcbc69b646abab
Cristian Paez <paezmartinez866@gmail.com>
2023-06-11 13:08:58 -0500

    Se crean variables y se asocian con la interfaz grafica

---
+++ Concesionario/src/main/java/Logica/AgregarVenta.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+/**
+ *
+ * @author Cristian
+ */
+public class AgregarVenta {
+
+    public void insertDatos(String monto, String codigo, String nombre, String apellido, String documento, int PROPERTIES) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void insertDatos(String monto, String codigo, String nombre, String apellido, String documento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
