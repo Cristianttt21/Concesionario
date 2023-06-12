0681454ae1d604379259cf2e4288562f305cf22d
rzl10 <rzl10@Lenovo>
2023-06-12 17:28:18 -0500

    Se incluyo form y Clase para actualizacion de datos, la cual incluye el reporte de los registros ingresados, la idea es que su pueda modificar seleccionando un registro de la grilla.

--- Concesionario/src/main/java/Formularios/Frm_ActualizarVehiculo.java
+++ Concesionario/src/main/java/Formularios/Frm_ActualizarVehiculo.java
@@ -3,20 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Formularios;
-
+import Logica.Cls_ActualizarVehiculo;
 /**
  *
  * @author Rodrigo Zuñiga
  */
 public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
-
+    int num = 0;
+    private final Cls_ActualizarVehiculo CP;
     /**
      * Creates new form Frm_ActualizarVehiculo
      */
     public Frm_ActualizarVehiculo() {
         initComponents();
+        CP = new Cls_ActualizarVehiculo();
+        listar();
+        this.setLocationRelativeTo(null);
     }
 
+    private void listar (){
+        Jtb_datos.setModel(CP.getDatos());
+    }
+    
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -30,12 +39,12 @@ public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         Jtf_Placa = new javax.swing.JTextField();
-        Jtf_Marca = new javax.swing.JTextField();
-        Jtf_Tipo = new javax.swing.JTextField();
         Jtf_Modelo = new javax.swing.JTextField();
         Jtf_Kilometraje = new javax.swing.JTextField();
+        jcb_tipo = new javax.swing.JComboBox<>();
+        Jcb_Marca = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        Jtb_datos = new javax.swing.JTable();
         Btn_Actualizar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -79,11 +88,16 @@ public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
 
         jLabel5.setText("Kilometraje");
 
-        Jtf_Marca.addActionListener(new java.awt.event.ActionListener() {
+        Jtf_Placa.setEditable(false);
+
+        jcb_tipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Automovil", "Camioneta", "Motocicleta ", "Motocarro", "Camión", "Volqueta", "Tractocamión", "Bus", "Microbús", "Limusina" }));
+        jcb_tipo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Jtf_MarcaActionPerformed(evt);
+                jcb_tipoActionPerformed(evt);
             }
         });
+
+        Jcb_Marca.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Acura", "Alfa Romeo", "Audi", "Baic", "BAW", "BMW", "Brilliance", "BYD", "Cadillac", "Changan", "Changhe", "Chery", "Chevrolet", "Chrysler", "Daihatsu", "DFSK", "Dodge", "Dongfeng", "DS", "FAW", "Ferrari", "Fiat", "Ford", "Foton", "Geely", "GMC", "Gonow", "Great Wall", "Haima", "Haval", "Honda", "Hummer", "Hyundai", "Infiniti", "JAC", "Jaguar", "Jeep", "Kenbo", "Kia", "Land Rover", "Lexus", "Lifan", "Mahindra", "Maserati", "Maxus", "Mazda", "Mercedes-Benz", "MG", "Mini", "Mitsubishi", "Nissan", "Opel", "Otros", "Peugeot", "Porsche", "RAM", "Renault", "SEAT", "Skoda", "Ssangyong", "Subaru", "Suzuki", "Toyota", "UAZ", "Volkswagen", "Volvo", "Wuling", "Zhongxing", "Zotye" }));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -96,24 +110,24 @@ public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Jtf_Marca, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Jtf_Placa, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(71, 71, 71)
+                    .addComponent(Jtf_Placa, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Jcb_Marca, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(78, 78, 78)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3)
                     .addComponent(jLabel4))
                 .addGap(34, 34, 34)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(Jtf_Tipo, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(Jtf_Modelo, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel5)
                         .addGap(31, 31, 31)
                         .addComponent(Jtf_Kilometraje, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(91, 91, 91))))
+                        .addGap(91, 91, 91))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jcb_tipo, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -123,32 +137,42 @@ public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jLabel3)
                     .addComponent(Jtf_Placa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Jtf_Tipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jcb_tipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(21, 21, 21)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jLabel4)
                     .addComponent(jLabel5)
-                    .addComponent(Jtf_Marca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Jtf_Modelo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Jtf_Kilometraje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(Jtf_Kilometraje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Jcb_Marca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(23, Short.MAX_VALUE))
         );
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        Jtb_datos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Placa", "Marca", "Tipo", "Modelo", "Kilometraje"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        Jtb_datos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                Jtb_datosMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(Jtb_datos);
 
         Btn_Actualizar.setText("Actualizar");
+        Btn_Actualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Btn_ActualizarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -182,13 +206,36 @@ public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void Actualizar(){
+        if (num == 0){
+            
+        }else {
+            
+        }
+    }
+    
+    
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
 
-    private void Jtf_MarcaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Jtf_MarcaActionPerformed
+    private void Jtb_datosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jtb_datosMouseClicked
         // TODO add your handling code here:
-    }//GEN-LAST:event_Jtf_MarcaActionPerformed
+        int row = Jtb_datos.getSelectedRow();
+        Jtf_Placa.setText(Jtb_datos.getValueAt(row, 0).toString());
+        jcb_tipo.setSelectedItem(Jtb_datos.getValueAt(row, 1).toString());
+        Jcb_Marca.setSelectedItem(Jtb_datos.getValueAt(row, 2).toString());
+        Jtf_Modelo.setText(Jtb_datos.getValueAt(row, 3).toString());
+        Jtf_Kilometraje.setText(Jtb_datos.getValueAt(row, 4).toString());        
+    }//GEN-LAST:event_Jtb_datosMouseClicked
+
+    private void jcb_tipoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcb_tipoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jcb_tipoActionPerformed
+
+    private void Btn_ActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn_ActualizarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_Btn_ActualizarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -197,11 +244,11 @@ public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Btn_Actualizar;
+    private javax.swing.JComboBox<String> Jcb_Marca;
+    private javax.swing.JTable Jtb_datos;
     private javax.swing.JTextField Jtf_Kilometraje;
-    private javax.swing.JTextField Jtf_Marca;
     private javax.swing.JTextField Jtf_Modelo;
     private javax.swing.JTextField Jtf_Placa;
-    private javax.swing.JTextField Jtf_Tipo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -210,7 +257,7 @@ public class Frm_ActualizarVehiculo extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JComboBox<String> jcb_tipo;
     // End of variables declaration//GEN-END:variables
 }
